5d5fdbf593ce36984f7a7964615c38d7199eadfb
yleosamp <leonnardomelo3@gmail.com>
2023-09-18 12:23:43 -0300

    Init Commit

---
+++ main.js
@@ -0,0 +1,5 @@
+import '/public/styles/style.css'
+import '/public/styles/fonts.css'
+import '/public/styles/reset.css'
+
+import '/public/scripts/rive.js'
---
+++ public/scripts/rive.js
@@ -0,0 +1,10 @@
+import rive from "@rive-app/canvas";
+
+const r = new rive.Rive({
+  src: "", // Colocar link do arquivo .riv (ou local)
+  canvas: document.getElementById("canvas"),
+  autoplay: true,
+  onLoad: () => {
+    r.resizeDrawingSurfaceToCanvas()
+  },
+})
